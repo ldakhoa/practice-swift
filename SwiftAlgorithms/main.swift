f0f27af035ebfff47fec9f58976fde57c5339d3b
ldakhoa <ldakhoa1308@gmail.com>
2019-10-30 18:28:07 +0700

    Initial Commit

---
+++ SwiftAlgorithms/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftAlgorithms
+//
+//  Created by Khoa Le on 10/30/19.
+//  Copyright © 2019 Khoa Le. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
